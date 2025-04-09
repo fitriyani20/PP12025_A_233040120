fe6b8f6288dfde0e0a2ce06c0fbc4038549e62e5
fitriyani20 <147709585+fitriyani20@users.noreply.github.com>
2025-04-09 14:59:50 +0700

    Add files via upload

---
+++ Pertemuan5/Node.java
@@ -0,0 +1,19 @@
+package Pertemuan5;
+
+class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+}
