de5cbe65c910b9be45888ac408ff270f4effbcf2
fitriyani20 <147709585+fitriyani20@users.noreply.github.com>
2025-05-21 14:24:51 +0700

    Add files via upload

---
+++ Pertemuan10/AbstractStack.java
@@ -0,0 +1,8 @@
+package Pertemuan10;
+
+
+	abstract class AbstractStack {
+	    // Abstract method to be implemented by subclasses
+	    abstract void displayStack(String s);
+	    abstract int peekN(int n);
+	}
